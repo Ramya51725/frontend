196b90a8bcab1a5519aa7cb10dfdf1ce44832fd7
Ramya51725 <radhi240308@gmail.com>
2026-01-31 15:30:49 +0530

    folder structure

---
+++ fitzy_frontend/javascript/home.js
@@ -0,0 +1,14 @@
+const categoryId = Number(localStorage.getItem("category_id"));
+const categorySpan = document.getElementById("categoryName");
+
+  if (!categorySpan) return;
+
+  if (categoryId === 1) {
+    categorySpan.innerText = "Underweight";
+  } else if (categoryId === 2) {
+    categorySpan.innerText = "Normal weight";
+  } else if (categoryId === 3) {
+    categorySpan.innerText = "Overweight";
+  } else {
+    categorySpan.innerText = "Unknown";
+  }
